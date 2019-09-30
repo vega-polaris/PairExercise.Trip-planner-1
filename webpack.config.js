535927badca2973733cca0a3b093db1c61c8e4a4
Featherweight <vais.emma@gmail.com>
2019-09-30 11:34:06 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development", // "production" | "development" | "none"
+  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  entry: "./src/index.js", // string | object | array
+  // defaults to ./src
+  // Here the application starts executing
+  // and webpack starts bundling
+  output: {
+    // options related to how webpack emits results
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  }
+};
